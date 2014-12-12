e71f015e8d36ac2d59bc4b121aa5d920840d07c5
Rafael Lara <RafaelLara@eprosima.com>
2014-12-12 10:36:37 +0100

    Added function to get java variable name
    Added method getJavaName to obtein the name of the attribute with the
    first letter capitalized.

--- src/main/java/com/eprosima/idl/parser/typecode/Member.java
+++ src/main/java/com/eprosima/idl/parser/typecode/Member.java
@@ -16,6 +16,18 @@ public class Member
     {
         return m_name;
     }
+    
+    public String getJavaName() {
+    	if (m_name != null) {
+	    	Character firstChar =Character.toUpperCase(m_name.charAt(0));
+	    	String javaName = firstChar.toString();
+	    	if (m_name.length() > 1) {
+	    		javaName += m_name.substring(1);
+	    	}
+	    	return javaName;
+    	}
+    	return null;
+    }
 
     /*
      * @brief This function is used with RTI DDS types because array names contains [].
