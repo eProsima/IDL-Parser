1830a95d062ec369b14c6b6c2f1b61695bbe5053
Ricardo González Moreno <correoricky@gmail.com>
2018-07-20 07:09:52 +0200

    Added C scoped name for structures.

--- src/main/java/com/eprosima/idl/parser/tree/TreeNode.java
+++ src/main/java/com/eprosima/idl/parser/tree/TreeNode.java
@@ -25,35 +25,43 @@ public class TreeNode implements Notebook
 {
     public TreeNode(String scopeFile, boolean isInScope, String scope, String name, Token tk)
     {
-		m_scopeFile = scopeFile;	
-		m_isinscope = isInScope;
+        m_scopeFile = scopeFile;	
+        m_isinscope = isInScope;
         m_name = name;
         m_scope = scope;
         m_annotations = new HashMap<String, Annotation>();
         tk_ = tk;
     }
 
-	public String getScopeFile()
-	{
-		return m_scopeFile;
-	}
-	
-	public boolean isInScope()
-	{
-		return m_isinscope;
-	}
+    public String getScopeFile()
+    {
+        return m_scopeFile;
+    }
+
+    public boolean isInScope()
+    {
+        return m_isinscope;
+    }
 
     public String getName()
     {
         return m_name;
     }
-    
+
     public String getScopedname()
     {
         if(m_scope == null || m_scope.isEmpty())
             return m_name;
 
         return m_scope + "::" + m_name;
+    }
+
+    public String getCScopedname()
+    {
+        if(m_scope.isEmpty())
+            return m_name;
+
+        return m_scope.replace("::", "_") + "_" + m_name;
     }
 
     public String getJavaScopedname()
@@ -68,7 +76,7 @@ public class TreeNode implements Notebook
     {
         return m_scope;
     }
-    
+
     /*
      * @brief This function returns the scoped name of the interface but
      * changing "::" by "_".
@@ -76,12 +84,12 @@ public class TreeNode implements Notebook
     public String getFormatedScopedname()
     {
         String ret = null;
-        
+
         if(m_scope == null || m_scope.isEmpty())
             ret = m_name;
         else
             ret = m_scope + "::" + m_name;
-        
+
         return ret.replaceAll("::", "_");
     }
 
@@ -96,7 +104,7 @@ public class TreeNode implements Notebook
         if(annotation != null)
             m_annotations.put(annotation.getName(), annotation);
     }
-    
+
     @Override
     public Map<String, Annotation> getAnnotations()
     {
@@ -107,9 +115,9 @@ public class TreeNode implements Notebook
     {
         return tk_;
     }
-	
-	private String m_scopeFile = null;
-	private boolean m_isinscope = false;
+
+    private String m_scopeFile = null;
+    private boolean m_isinscope = false;
     private String m_name = null;
     private String m_scope = null;
     //! Map that stores the annotations of the interface.
--- src/main/java/com/eprosima/idl/parser/typecode/MemberedTypeCode.java
+++ src/main/java/com/eprosima/idl/parser/typecode/MemberedTypeCode.java
@@ -19,7 +19,7 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 
 public abstract class MemberedTypeCode extends TypeCode
-{    
+{
     protected MemberedTypeCode(int kind, String scope, String name)
     {
         super(kind);
@@ -27,18 +27,26 @@ public abstract class MemberedTypeCode extends TypeCode
         m_name = name;
         m_members = new LinkedHashMap<String, Member>();
     }
-    
+
     public String getName()
     {
         return m_name;
     }
-    
+
     public String getScopedname()
     {
         if(m_scope.isEmpty())
             return m_name;
 
         return m_scope + "::" + m_name;
+    }
+
+    public String getCScopedname()
+    {
+        if(m_scope.isEmpty())
+            return m_name;
+
+        return m_scope.replace("::", "_") + "_" + m_name;
     }
 
     public String getJavaScopedname()
@@ -66,12 +74,12 @@ public abstract class MemberedTypeCode extends TypeCode
     {
         return !m_scope.isEmpty();
     }
-    
+
     public List<Member> getMembers()
     {
         return new ArrayList<Member>(m_members.values());
     }
-    
+
     public boolean addMember(Member member)
     {
         if(!m_members.containsKey(member.getName()))
@@ -81,22 +89,22 @@ public abstract class MemberedTypeCode extends TypeCode
         }
         return false;
     }
-    
+
     @Override
     public abstract String getCppTypename();
-    
+
     @Override
     public abstract String getCTypename();
 
     @Override
     public abstract String getJavaTypename();
-    
+
     @Override
     public abstract String getIdlTypename();
-    
+
     private String m_name = null;
-    
+
     private String m_scope = null;
-    
+
     private LinkedHashMap<String, Member> m_members = null;
 }
