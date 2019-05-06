5d9e0f3f96121cfa77abaf30bad34df48f2bc824
Ricardo González <correoricky@gmail.com>
2019-05-06 08:29:33 +0200

    Refs #5269. Fixed errors checking identifiers. (#17)

--- src/main/java/com/eprosima/idl/parser/tree/Definition.java
+++ src/main/java/com/eprosima/idl/parser/tree/Definition.java
@@ -16,19 +16,29 @@ package com.eprosima.idl.parser.tree;
 
 public interface Definition
 {
+    enum Kind
+    {
+        MODULE,
+        INTERFACE,
+        EXCEPTION,
+        TYPE_DECLARATION,
+        CONST_DECLARATION,
+        ANNOTATION
+    };
+
     void setParent(Object obj);
-    
+
     Object getParent();
-    
+
     public boolean isIsModule();
-    
+
     public boolean isIsInterface();
-    
+
     public boolean isIsException();
-    
+
     public boolean isIsTypeDeclaration();
 
     public boolean isIsConstDeclaration();
-	
+
     public boolean isIsAnnotation();
 }
