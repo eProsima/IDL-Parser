e65a50c69b22821f27c6ea7dd4589926ed13e10c
Javier Santiago <javiersantiago@eprosima.com>
2023-02-23 15:46:51 +0100

    Removed unused import
    Signed-off-by: Javier Santiago <javiersantiago@eprosima.com>

--- src/main/java/com/eprosima/idl/parser/typecode/BitmaskTypeCode.java
+++ src/main/java/com/eprosima/idl/parser/typecode/BitmaskTypeCode.java
@@ -16,7 +16,6 @@ package com.eprosima.idl.parser.typecode;
 
 import com.eprosima.idl.parser.exception.ParseException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Set;
