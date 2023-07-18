a0b42b5e40b34784fc13feac2b3d46c8e7f2231b
Mike Gratton <michael.b.gratton@gmail.com>
2023-07-17 20:46:42 -0400

    Add isIsEnumType() for use in StringTemplate

--- src/main/java/com/eprosima/idl/parser/typecode/TypeCode.java
+++ src/main/java/com/eprosima/idl/parser/typecode/TypeCode.java
@@ -224,6 +224,11 @@ public abstract class TypeCode implements Notebook
         return false;
     }
 
+    public boolean isIsEnumType()
+    {
+        return false;
+    }
+
     public boolean isIsSetType()
     {
         return false;
