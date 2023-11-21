c8fbc13934f486ca4667bfdc9682890efeba04a2
JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>
2023-11-21 08:53:26 +0100

    Refs #19932: apply review suggestions
    Signed-off-by: JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>

--- src/main/java/com/eprosima/idl/parser/typecode/UnionTypeCode.java
+++ src/main/java/com/eprosima/idl/parser/typecode/UnionTypeCode.java
@@ -32,7 +32,7 @@ public class UnionTypeCode extends MemberedTypeCode
             String name)
     {
         super(Kind.KIND_UNION, scope, name);
-        m_discriminator = null;
+        discriminator_ = null;
     }
 
     public UnionTypeCode(
@@ -41,15 +41,15 @@ public class UnionTypeCode extends MemberedTypeCode
             TypeCode discriminatorTypeCode)
     {
         super(Kind.KIND_UNION, scope, name);
-        m_discriminator = new UnionMember(discriminatorTypeCode, "discriminator", null, false);
-        m_discriminator.setId(0);
+        discriminator_ = new UnionMember(discriminatorTypeCode, "discriminator", null, false);
+        discriminator_.setId(0);
     }
 
     public void setDiscriminatorType(
             TypeCode discriminatorTypeCode)
     {
-        m_discriminator = new UnionMember(discriminatorTypeCode, "discriminator", null, false);
-        m_discriminator.setId(0);
+        discriminator_ = new UnionMember(discriminatorTypeCode, "discriminator", null, false);
+        discriminator_.setId(0);
     }
 
     @Override
@@ -93,10 +93,10 @@ public class UnionTypeCode extends MemberedTypeCode
         List<String> labels = null;
         List<String> javalabels = null;
 
-        if (Kind.KIND_ENUM == m_discriminator.getTypecode().getKind() ||
-                Kind.KIND_BITMASK == m_discriminator.getTypecode().getKind())
+        if (Kind.KIND_ENUM == discriminator_.getTypecode().getKind() ||
+                Kind.KIND_BITMASK == discriminator_.getTypecode().getKind())
         {
-            MemberedTypeCode enum_type = (MemberedTypeCode)m_discriminator.getTypecode();
+            MemberedTypeCode enum_type = (MemberedTypeCode)discriminator_.getTypecode();
             labels = new ArrayList<String>();
             javalabels = new ArrayList<String>();
 
@@ -190,9 +190,9 @@ public class UnionTypeCode extends MemberedTypeCode
     }
 
     // Used in stringtemplates
-    public TypeCode getDiscriminator()
+    public UnionMember getDiscriminator()
     {
-        return m_discriminator.getTypecode();
+        return discriminator_;
     }
 
     // Used in stringtemplates
@@ -261,7 +261,7 @@ public class UnionTypeCode extends MemberedTypeCode
         super.addAnnotation(ctx, annotation);
     }
 
-    private UnionMember m_discriminator = null;
+    private UnionMember discriminator_ = null;
 
     private int m_defaultindex = -1;
 
