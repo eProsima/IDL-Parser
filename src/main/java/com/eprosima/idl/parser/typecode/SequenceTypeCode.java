6e902f3f165d0794a0090bf15a599b902cc89344
Ricardo González <ricardo@richiware.dev>
2023-11-30 16:25:08 +0100

    Changeable max size for unbounded containers (#111)
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>

--- src/main/java/com/eprosima/idl/parser/typecode/SequenceTypeCode.java
+++ src/main/java/com/eprosima/idl/parser/typecode/SequenceTypeCode.java
@@ -113,7 +113,7 @@ public class SequenceTypeCode extends ContainerTypeCode
     {
         if (m_maxsize == null)
         {
-            return "100";
+            return default_unbounded_max_size;
         }
 
         return m_maxsize;
