ac469b8846031367109ed03fc96844e6eb06c6b6
Ricardo González Moreno <ricardo@richiware.dev>
2024-09-06 12:54:52 +0200

    Refs #21594. Apply suggestion
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>

--- src/main/java/com/eprosima/idl/parser/typecode/UnionTypeCode.java
+++ src/main/java/com/eprosima/idl/parser/typecode/UnionTypeCode.java
@@ -188,7 +188,7 @@ public class UnionTypeCode extends MemberedTypeCode
         {
             return getMembers().get(m_defaultindex);
         }
-        else if((Kind.KIND_BOOLEAN == discriminator_.getTypecode().getKind() && 2 == getMembers().size()) ||
+        else if((Kind.KIND_BOOLEAN == discriminator_.getTypecode().getKind() && 2 == getTotallabels().size()) ||
                 (Kind.KIND_ENUM == discriminator_.getTypecode().getKind() &&
                  ((EnumTypeCode)discriminator_.getTypecode()).getMembers().size() == getTotallabels().size()))
         {
