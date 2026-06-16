7dda6d51e37aedeb9934c61a927b539baad1f6cf
Ricardo González Moreno <ricardo@richiware.dev>
2026-06-16 07:59:40 +0200

    Refs #24619. Fix error reading octet number
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>

--- src/main/java/com/eprosima/idl/parser/tree/AnnotationMember.java
+++ src/main/java/com/eprosima/idl/parser/tree/AnnotationMember.java
@@ -140,7 +140,7 @@ public class AnnotationMember
                     // If it's hexadecimal, parse it using parseInt with radix 16
                     return Integer.toString(Integer.parseInt(m_value.substring(2), 16));
                 }
-                else if (m_value.startsWith("0"))
+                else if (m_value.startsWith("0") && m_value.length() > 1)
                 {
                     return Integer.toString(Integer.parseInt(m_value.substring(1), 8));
                 }
@@ -220,7 +220,7 @@ public class AnnotationMember
                         // If it's hexadecimal, parse it using parseInt with radix 16
                         return Integer.toString(Integer.parseInt(m_value.substring(2), 16));
                     }
-                    else if (m_value.startsWith("0"))
+                    else if (m_value.startsWith("0") && m_value.length() > 1)
                     {
                         return Integer.toString(Integer.parseInt(m_value.substring(1), 8));
                     }
